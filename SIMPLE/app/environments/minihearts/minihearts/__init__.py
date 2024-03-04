9814c5c5a259ca2bf6461c0025c04a0fa38dd72e
Samwise-B <sam.burcher99@gmail.com>
2024-03-04 16:18:03 +0000

    updated minihearts env

--- SIMPLE/app/environments/minihearts/minihearts/__init__.py
+++ SIMPLE/app/environments/minihearts/minihearts/__init__.py
@@ -1,7 +1,7 @@
 from gym.envs.registration import register
 
 register(
-    id='Hearts-v0',
-    entry_point='hearts.envs:HeartsEnv',
+    id='MiniHearts-v0',
+    entry_point='hearts.envs:MiniHeartsEnv',
     max_episode_steps=1,
 )
--- SIMPLE/app/environments/minihearts/setup.py
+++ SIMPLE/app/environments/minihearts/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='hearts',
     version='0.1.0',
-    description='Hearts Gym Environment',
+    description='Mini Hearts Gym Environment',
     packages=find_packages(),
     install_requires=[
         'gym>=0.9.4,<=0.15.7',
